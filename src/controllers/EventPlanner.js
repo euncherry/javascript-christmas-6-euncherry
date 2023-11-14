8f3ae1df417e26f4c108f51709153383ddc78db2
euncherry <csmo2642@naver.com>
2023-11-15 04:32:43 +0900

    feat: 고객에게 적용된 혜택 내역 계산 및 출력 구현

--- src/controllers/EventPlanner.js
+++ src/controllers/EventPlanner.js
@@ -50,10 +50,77 @@ class EventPlannerController {
     // this.setOrderListManager(selectedVisitDate, selectedMenu);
   }
 
+  checkAppliedEvents() {
+    const visitDate = this.#orderListManager.getVisitDate();
+    const orderListArray = this.#orderListManager.getOrdersArray();
+    const orderTotalPrice = this.#orderListManager.calculateOrderTotalPrice();
+    const giftMenu = this.#menuList.findMenuByName('샴페인');
+
+    this.#eventManager = new EventManager(
+      visitDate,
+      orderListArray,
+      orderTotalPrice,
+      giftMenu,
+    );
+  }
+
+  printPreviewMessage() {
+    const visitDate = this.#orderListManager.getVisitDate();
+    OutputView.printPreviewMessage(visitDate);
+  }
+  printPreOrderPreview() {
+    this.printMenu();
+    this.printTotalPriceBeforeDiscount();
+  }
+
+  printMenu() {
+    const orderMenuResult = this.#orderListManager.getOrderMenuNameCount();
+    OutputView.printMenu(orderMenuResult);
+  }
+
+  printTotalPriceBeforeDiscount() {
+    const originalTotalPriceResult = this.#orderListManager.calculateOrderTotalPrice();
+    OutputView.printTotalPriceBeforeDiscount(originalTotalPriceResult);
+  }
+
+  printGiftMenu() {
+    const giftMenuResult = this.#eventManager.calculateGiftMenu();
+    OutputView.printGiftMenu(giftMenuResult);
+  }
+
+  printBenefit() {
+    const totalBenefitsAmountResult = this.#eventManager.calculateTotalBenefits();
+    const benefitDetailResult = this.#eventManager.calculateBenefits();
+
+    OutputView.printBenefitDetail(benefitDetailResult, totalBenefitsAmountResult);
+    OutputView.printTotalBenefitsAmount(totalBenefitsAmountResult);
+  }
+
+  printFinalExpectPrice() {
+    const originalTotalPriceResult = this.#orderListManager.calculateOrderTotalPrice();
+    const finalExpectPriceResult = this.#eventManager.calculateDiscountedPayment(
+      originalTotalPriceResult,
+    );
+
+    OutputView.printFinalExpectPrice(finalExpectPriceResult);
+  }
+
+  printEventBenefitPreview() {
+    this.printGiftMenu();
+    this.printBenefit();
+    this.printFinalExpectPrice();
+  }
+
   async start() {
     OutputView.printWelcome();
 
     await this.createPreOrder();
+
+    this.checkAppliedEvents();
+
+    this.printPreviewMessage();
+    this.printPreOrderPreview();
+    this.printEventBenefitPreview();
   }
 
   getMenuManager() {
